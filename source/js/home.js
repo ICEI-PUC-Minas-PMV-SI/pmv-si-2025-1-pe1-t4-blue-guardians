a84d007dd81eada4a421fd416bd5e84a279cde7b
Claire Hausman <claire.hausman.m@gmail.com>
2025-05-27 17:55:12 -0300

    Consulta de conteúdo por ID

--- source/js/home.js
+++ source/js/home.js
@@ -12,7 +12,7 @@ fetch("http://localhost:3000/contents")
         <div class="card-content">
           <h3>${item.titulo}</h3>
           <p>${item.texto.substring(0, 100)}...</p>  <!-- um resumo curto -->
-          <a href="detalhe.html?id=${item.id}" class="btn">Saber mais</a>
+          <a href="content.html?id=${item.id}" class="btn">Saber mais</a>
         </div>
       `;
 
@@ -80,4 +80,47 @@ fetch("http://localhost:3000/tabData")
     }
   })
   .catch(err => console.error("Erro ao carregar tabData:", err));
+
+// Conteúdos por ID
+const params = new URLSearchParams(window.location.search);
+const id = params.get('id');
+
+if (id) {
+fetch(`http://localhost:3000/contents/${id}`)
+    .then(response => {
+    if (!response.ok) throw new Error("Conteúdo não encontrado");
+    return response.json();
+    })
+    .then(data => {
+    const container = document.getElementById("detail-container");
+    const container_slides = document.getElementById("slides");
+    container.innerHTML = `
+        <h2 style="text-align: center;">${data.titulo}</h2>
+        <div style="display: flex; justify-content: center; align-items: center; margin: 20px 0;">
+            <img src="${data.imagem}" alt="${data.titulo}" style="max-width: 100%; height: 300px; border-radius: 2px; object-fit: cover;">
+        </div>
+        <p>${data.descricao}</p>
+    `;
+    container_slides.innerHTML = `
+    <div class="slide">
+      <iframe src="${data.video1}?autoplay=1" title="ECOSSISTEMAS AQUÁTICOS" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      <iframe src="${data.video1}" title="ECOSSISTEMAS AQUÁTICOS" allowfullscreen></iframe>
+    </div>
+    <div class="slide">
+        <iframe src="${data.video2}?autoplay=1" title="ECOSSISTEMAS AQUÁTICOS" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <iframe src="${data.video2}" title="ECOSSISTEMAS AQUÁTICOS" allowfullscreen></iframe>
+    </div>
+    <div class="slide">
+        <iframe src="${data.video3}?autoplay=1" title="ECOSSISTEMAS AQUÁTICOS" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <iframe src="${data.video3}" title="ECOSSISTEMAS AQUÁTICOS" allowfullscreen></iframe>
+    </div>
+    `;
+    })
+    .catch(error => {
+    document.getElementById("detail-container").innerHTML = "<p>Erro ao carregar conteúdo.</p>";
+    console.error("Erro:", error);
+    });
+} else {
+document.getElementById("detail-container").innerHTML = "<p>Conteúdo não especificado.</p>";
+}
 
